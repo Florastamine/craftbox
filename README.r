022dff0dab112d923ced6ba7ae0825fb369b5d17
Nguyễn Ngọc Huy <remilla.scarlet.94@gmail.com>
2013-06-16 18:13:34 +0700

    Create README.r

---
+++ README.r
@@ -0,0 +1,8 @@
+0
+=
+
+Edu6
+
++ Known Issues:
+
+- The engine gradually slows down when any entity is being selected, but back to normal when nothing is selected. This is a coding problem and I'm still working on it.
